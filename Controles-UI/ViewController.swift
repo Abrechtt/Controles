d3c3b2bc658747abdc68b6841bff8a43bc6b2d9b
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-08-15 08:51:21 -0700

    primero

---
+++ Controles-UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Controles-UI
+//
+//  Created by Alumno on 8/15/22.
+//  Copyright © 2022 Ulsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
